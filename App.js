d3f6e682eb05d89e5463c5d88bf1628c66757d0d
Matteo Contini <matteo.contini@zalando-lounge.de>
2016-11-02 17:39:46 +0100

    create a stateless function to replicate input field

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { txt: 'this is the state txt' }
+    this.state = { txt: '' }
+    this.update = this.update.bind(this)
   }
 
   update(e){
@@ -14,12 +15,20 @@ class App extends React.Component {
   render() {
     return (
     <div>
-      <input type="text"
-        onChange={this.update.bind(this)} />
-      <h1>{this.state.txt}</h1>
+      <Widget txt={this.state.txt} update={this.update} />
     </div>
   );
   }
+}
+
+const Widget = (props) => {
+  return (
+  <div>
+    <input type="text"
+      onChange={props.update} />
+    <h1>{props.txt}</h1>
+  </div>
+);
 }
 
 export default App
