139784b4918001b5a96308a8aa7c7bfe5ee448a3
Matteo Contini <matteo.contini@zalando-lounge.de>
2016-11-02 16:26:17 +0100

    added setState and notes on learning

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { txt: 'this is the state txt' }
+  }
+
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+
   render() {
-    let txt = this.props.txt;
-    let cat = this.props.cat
     return (
-      <div>
-        <h1>{txt}</h1>
-        <b>{cat}</b>
-      </div>
+    <div>
+      <input type="text"
+        onChange={this.update.bind(this)} />
+      <h1>{this.state.txt}</h1>
+    </div>
   );
   }
 }
 
-// // ora vado a fare la stessa cosa usando una STATELESS FUNCTION il metodo usato da Atharva
-// come dice il nome UNA STATELLS non puó avere stato
-// const App = () => <h1>HELLO STATELESS FUNCTION</h1>
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
-ReactDOM.render(
-  <App cat={5} txt="this is the props value" />, document.getElementById('app')
-);
+export default App
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 to exit from git diff use q
 
 I try now to do some change just to understand the flow
+
+with >git reset< I can reset a git add before commit
