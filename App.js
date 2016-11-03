641330fcc4af280f8271ab398ba0cd1d6ac4872f
Matteo Contini <matteo.contini@zalando-lounge.de>
2016-11-03 20:11:43 +0100

    06 using of this.props.children to add value

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
 import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+    <Button>
+      React
+    </Button>
+  );
+  }
+}
+
+class Button extends React.Component {
+    render() {
+      return (
+      <button>
+        {this.props.children}
+      </button>
+    );  
+    }
+}
 
 export default App
